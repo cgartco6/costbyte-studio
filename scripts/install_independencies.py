771e534fb6823f156138471b01ee76327274b53b
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-03-03 19:57:06 +0200

    Create install_independencies.py

---
+++ scripts/install_independencies.py
@@ -0,0 +1,8 @@
+import os
+import platform
+
+os_type = platform.system().lower()
+os.system("pip install -r backend/requirements.txt")
+os.system("cd frontend && npm install")
+
+print("✅ Dependencies installed")
