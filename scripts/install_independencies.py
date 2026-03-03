367fcca1d65639c81a1a265ab97c058c3cfd4892
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-03-03 21:52:35 +0200

    Update install_independencies.py

--- scripts/install_independencies.py
+++ scripts/install_independencies.py
@@ -1,9 +1,4 @@
-# scripts/install_dependencies.py
 import os
-import platform
-
-os_type = platform.system().lower()
 os.system("pip install -r backend/requirements.txt")
 os.system("cd frontend && npm install")
-
-print("✅ Deps installed")
+print("Deps installed")
